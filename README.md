f7c248e2d0a7f0b29455bd08c3bb5f6e239c734e
daniel hinost <danielgibran18tj@hotmail.com>
2023-02-19 22:01:41 -0500

    descripcion del funcionamiento y correccion de detalles

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # escuelaspirit
 notas de alumnos de escuela
+
+Hola, bienvenido
+
+Este proyecto muestra las calificaciones de diferentes alumnos, puedes ingresar siendo el profesor, alumno o curso... al ingresar como profesor con su correo escoger el curso a observar y luego cual alumno ver las notas.
+Al ingresar como alumno escoger directamente la materia a ver tus notas. Al ingresar como curso, escoges que estudiante y observaras las notas de todas las materias de aquel estudiante.
+
+Este proyecto está orientado a objetos, cualquier sugerencia es bienvenida
+
+//DATOS DE PROFESORES
+
+alejandro martinez   -> alejandro@gmail.com
+
+Karen sosa   -> karen@gmail.com
+
+daniel Hinost   -> daniel@gmail.com
+
+//ALUMNOS DE QUINTO CURSO 
+
+juanito salgado   -> juanito@gmail.com
+
+pepito rodrigues   -> pepito@gmail.com
+
+carlitos sanchez   -> carlitos@gmail.com
+
+//ALUMNOS DE SEXTO CURSO
+
+oliver aton   -> oliver@gmail.com
+
+goku kakaroto   -> goku@gmail.com
+
+bulma saiyayin   -> bulma@gmail.com
+
+//ALUMNOS DE SEPTIMO CURSO
+
+gato perez   -> gato@gmail.com
+
+novita doraemon   -> novita@gmail.com
+
+pajaro loco   -> pajaro@gmail.com
--- ui/MenuAlumno.java
+++ ui/MenuAlumno.java
@@ -8,20 +8,20 @@ public class MenuAlumno {
         System.out.println("Bienvenido alumno " + UiMenu.alumnoLogueado.nombres +
                 " del curso " + UiMenu.alumnoLogueado.getCurso());
         do{
-            System.out.println("Ingresa nombre de la materia que quieres revisar:\n1. matematicas" +
+            System.out.println("Ingresa numero de la materia que quieres revisar:\n1. matematicas" +
                     "\n2. ciencias naturales\n3. estudios sociales\n4. ingles\n5. informatica\n0. Salir");
             Scanner sc = new Scanner(System.in);
             respuestaAlumno = Integer.valueOf(sc.nextLine());
             if (respuestaAlumno==1){
-                System.out.println("Matematicas: " + UiMenu.alumnoLogueado.matematicas);
+                System.out.println("Matematicas: " + UiMenu.alumnoLogueado.matematicas+"\n");
             } else if (respuestaAlumno==2) {
-                System.out.println("Ciencias Naturales: " + UiMenu.alumnoLogueado.ciencias_naturales);
+                System.out.println("Ciencias Naturales: " + UiMenu.alumnoLogueado.ciencias_naturales+"\n");
             } else if (respuestaAlumno==3) {
-                System.out.println("Estudios Sociales: " + UiMenu.alumnoLogueado.estudios_sociales);
+                System.out.println("Estudios Sociales: " + UiMenu.alumnoLogueado.estudios_sociales+"\n");
             }else if (respuestaAlumno==4) {
-                System.out.println("Ingles: " + UiMenu.alumnoLogueado.ingles);
+                System.out.println("Ingles: " + UiMenu.alumnoLogueado.ingles+"\n");
             }else if (respuestaAlumno==5) {
-                System.out.println("Informatica: " + UiMenu.alumnoLogueado.informatica);
+                System.out.println("Informatica: " + UiMenu.alumnoLogueado.informatica+"\n");
             }
         }while (respuestaAlumno !=0);
     }
